8da143a6ff6b986ce7a10078d79938d49b5e73cc
designer <avadakkeddavra@gmail.com>
2017-09-08 16:04:29 +0300

    add new sections

---
+++ js/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#testimonials_slider').slick({
+		arrows:true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		dots:true,
+		prevArrow:'<button type="button" class="slick-prev fa fa-angle-left"><i class="fa fa-angle-left"></i></button>',
+		nextArrow:'<button type="button" class="slick-next fa fa-angle-left"><i class="fa fa-angle-right"></i></button>',
+	})
+})
